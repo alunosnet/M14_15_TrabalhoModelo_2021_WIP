53deecac9dc0a21816a5bcda0cbfb20729b77b42
Paulo Ferreira <alunosnet@gmail.com>
2020-11-18 11:46:29 +0000

    Classe base de dados

--- M14_15_TrabalhoModelo_2021_WIP/BaseDados.cs
+++ M14_15_TrabalhoModelo_2021_WIP/BaseDados.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Text;
 
 namespace M14_15_TrabalhoModelo_2021_WIP
 {
     class BaseDados
     {
+        string BDName = "M14_15_TrabalhModelo_2021";
+        string caminho;
+        string strLigacao;
+        SqlConnection ligacaoBD;
+
+        public BaseDados()
+        {
+            //se a bd existe
+            //definir o caminho para bd
+            caminho = Utils.pastaDoPrograma() + @"\" + BDName + ".mdf";
+            if (File.Exists(caminho) == false)
+                criarBD();
+
+            //atualizar a string ligação
+            //abrir a ligação a bd
+
+        }
+
+        private void criarBD()
+        {
+            
+        }
+
+        ~BaseDados()
+        {
+
+        }
     }
 }
